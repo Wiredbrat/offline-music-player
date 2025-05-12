4e1f6525d0178fccf84a63667a4951925df6ae35
Wiredbrat <vermaatul291002@gmail.com>
2025-05-12 20:54:45 +0530

    Add files via upload

---
+++ music player/api.js
@@ -0,0 +1,168 @@
+const listOfTracks = [
+    {
+        name: "Kaho na kaho",
+        artist: "Amir Jamal",
+        url: "audio/Kaho Na Kaho.mp3",
+        duration: 311000,
+        image: "image/kaho-na-kaho.jpg",
+        trackId: 1,
+    },
+
+    {
+        name: "Ajab si",
+        artist: "KK",
+        url: "audio/Ajab Si.mp3",
+        duration: 243000,
+        image: "image/ajab-si.jpg",
+        trackId: 2,
+    },
+
+    {
+        name: "Tune Jo Na Kaha",
+        artist: "Mohit Chauhan",
+        url: "audio/Tune Jo Na Kaha.mp3",
+        duration: 309000,
+        image: "image/tune-jo-na-kaha.jpg",
+        trackId: 3,
+    },
+
+    {
+        name: "Khuda Jaane",
+        artist: "KK",
+        url: "audio/Khuda Jaane.mp3",
+        duration: 335000,
+        image: "image/khuda-jaane.jpg",
+        trackId: 4,
+    },
+
+    {
+        name: "Nadaan Parindey",
+        artist: "Mohit Chauhan",
+        url: "audio/Nadaan Parindey.mp3",
+        duration: 392000,
+        image: "image/nadaan-parindey.jpg",
+        trackId: 5,
+    },
+
+    {
+        name: "Ada",
+        artist: "Sonu Nigam",
+        url: "audio/Ada Original.mp3",
+        duration: 287000,
+        image: "image/ada.jpg",
+        trackId: 6,
+    },
+
+    {
+        name: "Chaiyya Chaiyya",
+        artist: "Sukhwinder Singh",
+        url: "audio/Chal Chaiya Chaiya.mp3",
+        duration: 416000,
+        image: "image/chaiyya-chaiyya.jpg",
+        trackId: 7,
+    },
+
+    {
+        name: "Phir Se Udd Chala",
+        artist: "Mohit Chauhan",
+        url: "audio/Phir Se Ud Chala.mp3",
+        duration: 271000,
+        image: "image/phir-se-udd-chala.jpg",
+        trackId: 8,
+    },
+
+    {
+        name: "Ashiq Banaya Aapne",
+        artist: "Himesh Reshamiya",
+        url: "audio/Aashiq Banaya Aapne.mp3",
+        duration: 363000,
+        image: "image/aashiq-banaya-aapne.jpg",
+        trackId: 9,
+    },
+
+    {
+        name: "Tum Hi Ho",
+        artist: "Arijit Singh",
+        url: " audio/Tum Hi Ho.mp3",
+        duration: 262000,
+        image: "image/tum-hi-ho.jpg",
+        trackId: 10,
+    },
+
+    {
+        name: "Ilahi",
+        artist: "Mohit Chauhan",
+        url: "audio/Ilahi.mp3",
+        duration: 229000,
+        image: "image/ilahi.jpg",
+        trackId: 11,
+    },
+
+    {
+        name: "Koi Fariyad",
+        artist: "Jagjit Singh",
+        url: "audio/Koi Fariyaad.mp3",
+        duration: 541000,
+        image: "image/koi-fariyaad.jpg",
+        trackId: 12,
+    },
+
+    {
+        name: "Woh Lamhe",
+        artist: "Atif Aslam",
+        url: " audio/Woh Lamhe Woh Baatein.mp3",
+        duration: 320000,
+        image: "image/woh-lamhe.jpg",
+        trackId: 13,
+
+    },
+
+    {
+        name: "Mast Magan",
+        artist: "Arijit Singh",
+        url: "audio/Mast Magan.mp3",
+        duration: 280000,
+        image: "image/mast-magan.jpg",
+        trackId: 14,
+
+    },
+
+    {
+        name: "Tu Hai Ke Nahi",
+        artist: "Ankit Tiwari",
+        url: "audio/Tu Hai Ki Nahi.mp3",
+        duration: 333000,
+        image: "image/tu-hai-ke-nahi.jpg",
+        trackId: 15,
+    },
+
+    {
+        name: "Teri Yaadon Mein",
+        artist: "KK",
+        url: "audio/Teri Yaadon Mein.mp3",
+        duration: 288000,
+        image: "image/teri-yaadeon-mein.jpg",
+        trackId: 16,
+    },
+
+    {
+        name: "Unknown",
+        artist: "Unknown",
+        url: "audio/Voicy_Ara ara meme.mp3",
+        duration: 12000,
+        image: "",
+        trackId: 17,
+    },
+
+    {
+        name: "Aap Baithe Hai Balin Pe Meri",
+        artist: "Nusrat Fateh Ali Khan",
+        url: "audio/Aap Bethe Balain Peh.mp3",
+        duration: 941000,
+        image: "image/aap-baithe-h-balin-pe.jpg",
+        trackId: 18,
+    },
+
+];
+
+export {listOfTracks};
---
+++ script.js
@@ -0,0 +1,326 @@
+import {listOfTracks} from "./api.js";  // to import the music list 
+
+// console.log(listOfTracks[1].name);
+
+// to create random colors to styling the background when song changes
+const randomBg = () => {
+    let code = "";
+    let hex = "9876543210abcdef";
+    if(code === "") {
+        for(let x = 0; x < 8; x++) {
+            let random = Math.floor(Math.random()*16);
+            code += hex[random];
+            document.querySelector(".wrapper").style.backgroundColor = `#${code}`; 
+        }
+        // console.log(code);
+    }
+    code = "";
+}
+
+
+const music = document.createElement("audio");  // added audio tag to play music in the app
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const playPause = document.getElementById("play-pause");
+const trackImage = document.querySelector(".image"); 
+
+let isPlaying = false;
+let index ;
+
+// music.autoplay = true;  
+
+function musicdata() {
+    title.innerHTML = `<span style="font-size:18px;">${listOfTracks[index].name}</span> <br> <span style="font-size:12px;">${listOfTracks[index].artist}</span>`;
+    trackImage.innerHTML = `<img src="${listOfTracks[index].image}">`;
+    music.src = listOfTracks[index].url;
+    music.play();
+    isPlaying =true;
+    randomBg();
+    musicDuration();
+}
+
+// functions for button events
+
+// to go to previous song
+const prevBtn = () => {
+    if(index > 0) {
+        index--;
+        musicdata();
+        callFunction();
+        playPause.src = "image/pause.png";
+
+    }
+}
+
+// to go to next song
+const nextBtn = () => {
+    if(index < listOfTracks.length - 1) {
+        index++
+        musicdata();
+        callFunction();
+        playPause.src = "image/pause.png";
+
+    }
+}
+
+// to play and pause the song
+const PlayPauseBtn = () => {
+    if(!isPlaying) {
+        if(index >= 0){
+            music.play();
+            isPlaying =true;
+            playPause.src = "image/pause.png";
+        }
+    }else{
+        music.pause()
+        isPlaying = false;
+        playPause.src = "image/play-button.png";
+    }
+}
+
+const musicList = document.querySelector(".list-items");
+const title = document.querySelector(".title");
+
+// accessing all the tracks available in api.js and storing it in lists
+listOfTracks.forEach((track) => {
+    const list = document.createElement("li");
+    list.innerHTML = `<span id=${track.trackId} style="display:content;">
+    <span style=""> <img src=${track.image} height="20px"></span>
+    <span style="font-size:18px;">${track.name}</span> <br> 
+    <span style="font-size:12px;">${track.artist}</span></span>`;
+    music.src = `${track.url}`;
+    musicList.appendChild(list);
+    
+})
+
+// music list event to play music and access related information by clicking on listitem
+musicList.addEventListener("click", (event) => {
+    if(event.target.tagName === "LI"){
+        title.innerHTML = event.target.innerHTML;
+        index = event.target.childNodes[0].attributes[0].value - 1;
+        music.src = listOfTracks[index].url;
+        trackImage.innerHTML = event.target.childNodes[0].children[0].innerHTML;
+        playPause.src = "image/pause.png";
+        music.play();
+        isPlaying =true;
+        musicDuration();
+        randomBg();
+        slideFunction();
+        callFunction();
+    }
+})
+
+const slideBtn = document.querySelector(".slide-button");
+// const slideRightBtn = document.querySelector(".slide-button1");
+
+// button events
+document.addEventListener("click", (e) => {
+    if(e.target === prev) {
+        prevBtn();
+    }
+    else if(e.target === next) {
+        nextBtn();
+    }
+    else if(e.target === playPause) {
+        PlayPauseBtn();
+        pauseTrackTime();
+        valueStore();
+    }
+    else if(e.target === slideBtn) {
+        slideFunction();
+    }
+})
+
+
+// to convert miliseconds to seconds and minutes:
+const totalDuration = document.querySelector(".total-duration"); 
+const runningDuration = document.querySelector(".running-duration"); 
+const seekBtn = document.getElementById("range");
+
+seekBtn.disabled = true;
+
+const musicDuration = () => {
+    let durationInSec = listOfTracks[index].duration/1000;
+    let minutes = Math.floor(durationInSec/60);
+    let seconds = Math.floor(durationInSec - minutes*60);
+    if(minutes > 9 && seconds > 9){
+       return totalDuration.innerHTML = `${minutes}:${seconds}`;
+    }
+    else if(minutes <= 9 && seconds > 9) {
+       return totalDuration.innerHTML = `0${minutes}:${seconds}`;
+    }
+    else if(minutes > 9 && seconds <= 9) {
+       return totalDuration.innerHTML = `${minutes}:0${seconds}`;
+    }
+    else{
+        return totalDuration.innerHTML = `0${minutes}:0${seconds}`;
+    }
+}
+
+// to create the running time duration of song
+let playTime; // this variable is made to store the current playing time of music do not change or assign value to it
+let sec = 0;
+let min = 0;
+const onseek = () => {
+playTime = setInterval(() =>{
+    if(sec < 59) {
+            if(sec < 9 && min < 10) {
+                sec++;
+                runningDuration.innerHTML = `0${min}:0${sec}`;
+            }
+            else if(sec >= 9 && min < 10) {
+                sec++;
+                runningDuration.innerHTML = `0${min}:${sec}`;
+            }
+            else if(sec < 10 && min > 10) {
+                sec++;
+                runningDuration.innerHTML = `${min}:0${sec}`;
+            }else{
+                sec++;
+                runningDuration.innerHTML = `${min}:${sec}`;
+            }
+        }else{
+            if(min < 10) {
+                min++;
+                runningDuration.innerHTML = `0${min}:00`;
+                sec = 0;
+            }else{
+                min++;
+                runningDuration.innerHTML = `${min}:00`;
+                sec = 0
+            }
+        }
+        
+    },1000)
+
+    setTimeout(() => {
+        clear();
+    },listOfTracks[index].duration);
+}
+
+
+// to clear the current played time of the music  
+const clear = () => {
+    clearInterval(playTime);
+    sec = 0;
+    min = 0;
+    runningDuration.innerHTML = `0${min}:0${sec}`;
+}
+    
+// to create silde function for music lists it works only in moblie version
+const listArea = document.querySelector(".lists");
+
+const slideFunction = () => {
+    listArea.classList.toggle("slideFunction"); 
+    slideBtn.classList.toggle("rotateBtn");
+}
+
+// to call out stack of functions in one go
+const callFunction = () => {
+    clear();
+    clearRange();
+    onseek();
+    seekValue();
+}
+
+//  this is the function to pause the track play time when the playPauseBtn is clicked
+
+const pauseTrackTime = () => {
+    let storeTime;
+    let timeElapsed;
+    if(!isPlaying) {
+        if(min < 10 && sec < 10){
+            storeTime = `0${min}:0${sec}`;
+        }
+        else if(min > 9 && sec < 10){
+            storeTime = `${min}:0${sec}`;
+        }
+        else if(min < 10 && sec > 9){
+            storeTime = `0${min}:${sec}`;
+        }
+        else if(min > 9 && sec > 9){
+            storeTime = `${min}:${sec}`;
+        }
+        timeElapsed = (min*60 + sec)*1000;
+        clearInterval(playTime);
+        runningDuration.innerHTML = storeTime;
+        // console.log(timeElapsed);
+        
+    }
+    else if(isPlaying) {
+        if(timeElapsed >= 1000) {
+            sec = timeElapsed/1000;
+            if(sec >= 58){
+                min = Math.floor(sec/60);
+                // sec = timeElapsed/1000 - min*60;    //not in use
+            }
+        }
+        
+        // console.log(min);
+        // console.log(sec);
+        if(min < 10 && sec < 10){
+            runningDuration.innerHTML = `0${min}:0${sec}`;
+        }
+        else if(min > 9 && sec < 10){
+            runningDuration.innerHTML = `${min}:0${sec}`;
+        }
+        else if(min < 10 && sec > 9){
+            runningDuration.innerHTML = `0${min}:${sec}`;
+        }
+        else if(min > 9 && sec > 9){
+            runningDuration.innerHTML = `${min}:${sec}`;
+        }
+        onseek();
+    }
+}
+
+
+// to attach the range value to the running time duration of song
+let rangeInterval;  // this variable is made to store the current playing time of music do not change or assign value to it
+let val = 0;
+let store = 0;
+const seekValue = () => {
+    rangeInterval = setInterval(() =>{
+        val++
+        if(seekBtn.value <=100){
+            seekBtn.value = store + val;
+        }
+        // console.log((listOfTracks[index].duration/100)/1000)
+    },listOfTracks[index].duration/100);
+
+    setTimeout(() => {
+        clearRange();    
+    },listOfTracks[index].duration)
+}
+   
+// to clear the current played time of the music on rangebar 
+const clearRange = () => {
+       clearInterval(rangeInterval);
+       val = 0;
+       seekBtn.value = val;
+}
+
+const valueStore = () => {
+    if(!isPlaying) {
+        store = val;
+        clearRange();
+        seekBtn.value = store;
+        console.log(store);
+    }else if(isPlaying) {
+        // val = store;
+        seekBtn.value = store + val; 
+        console.log(store);
+        seekValue();
+        console.log(seekBtn.value);
+    }
+}
+// store = 0;
+
+// if(music.ended){
+//     console.log(music.ended)
+//     // playPause.src = "image/play-button.png";
+//     nextBtn();
+// }
+
+// END OF CODE.
